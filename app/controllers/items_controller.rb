6a07c643d00a3c04f7b89f8441690309ac93b446
shanghaiellen <shanghaiellen@gmail.com>
2014-03-19 10:09:15 -0700

    adding to cart adds an item to the cart in the API, but then the items do not show up on Ember refresh

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
   def create
+    Item.create(
+      cart_id: item_params[:cart],
+      product_id: item_params[:product],
+      quantity: item_params[:quantity],
+      item_price: item_params[:item_price]
+    )
+    @cart = item_params[:cart]
     render '/cart/show'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:cart, :product, :quantity, :item_price)
   end
 end
