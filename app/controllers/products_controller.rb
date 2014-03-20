28eebb507953fef632b89ba1d229a45fc1ff87e2
shanghaiellen <shanghaiellen@gmail.com>
2014-03-20 11:06:34 -0700

    added and resolved edit, create, and destroy in products controller;

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def create
+    @product = Product.create(product_params)
+    render :show
+  end
+
+  def destroy
+    @product = Product.find(params[:id].to_i)
+    @product.destroy
+    render "index"
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:id, :name, :price, :image, :avatar, :description)
+  end
 end
